f3129d7b72e1a2ed346171ac18ecf8c4c9ed320b
beerwithstraw <agarwalp@tcd.ie>
2020-10-23 20:45:46 +0100

    QuickNotes
    New JavaScript project with added hyperlink feature within notes

--- countdownTimer/script.js
+++ countdownTimer/script.js
@@ -9,10 +9,12 @@ function countdown(){
     const bdayDate = new Date(ganjebday);
     const currentDate = new Date()
 
+    //from ms to sec
     const totalseconds = new Date(bdayDate - currentDate)/1000;
 
 
     const days = Math.floor(totalseconds / (3600*24));
+    //daylight saving
     const hours = (Math.floor(totalseconds/3600) %24) - 1;
     const minutes = Math.floor(totalseconds/60) %60;
     const seconds = Math.floor(totalseconds) %60;
